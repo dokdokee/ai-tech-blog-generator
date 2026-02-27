7a721e999c48a29dfee66adb5d0043bc23a3e3cd
dokdokeee <eastsea1025@naver.com>
2026-02-27 17:37:07 +0900

    Fix Editor import path in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import Editor from "./components/Editor"
+import Editor from "../components/Editor";
 
 
 export default function Login() {
